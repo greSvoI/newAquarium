eafe3b5e8d6bd4af98e716c22203fc4349f5a155
Сергей <serega.iva@outlook.com>
2020-12-07 19:47:56 +0300

    Почистил код

--- Fish.cs
+++ Fish.cs
@@ -24,7 +24,6 @@ namespace newAquarium
 					Picture.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
 					rotate = value; 
 				}
-				//else rotate = value;
 			} 
 		}
 		private int x, y;
@@ -46,7 +45,6 @@ namespace newAquarium
 			};
 		    Xx = Form1.random.Next(0, 2) == 0 ? x = -1 : x = 1;
 			Yy = Form1.random.Next(0, 2) == 0 ? y = -1 : y = 1;
-			//Rotate = Xx == -1 ? Rotate = false : Rotate = true;
 
 			Picture.Location = new Point(
 				Form1.random.Next(0,400-Picture.Width), Form1.random.Next(0,200 - Picture.Height));
@@ -59,8 +57,6 @@ namespace newAquarium
 			Xx = Xx < 0 ? Form1.random.Next(1, speed) : Form1.random.Next(-speed, -1);
 			Yy = Yy < 0 ? Form1.random.Next(1, speed) : Form1.random.Next(-speed, -1);
 			Rotate = Xx < 0 ? Rotate = false : Rotate = true;
-			//	if (Xx < 0) Rotate = false;
-			//	else Rotate = true;
 		}
 		public void Locate()
 		{
@@ -75,30 +71,12 @@ namespace newAquarium
 			if (Picture.Location.X + Picture.Width >= Picture.Parent.Width || Picture.Location.X <= 0)
 			{
 				Xx = -Xx;
-				//if (Xx < 0) Rotate = false;
-				//else Rotate = true;
 				Rotate = Xx < 0 ? Rotate = false : Rotate = true;
-				//Picture.Location = new Point(Picture.Location.X <= 0 ? 0 : Picture.Parent.Width - Picture.Width, Picture.Location.Y);
-				//Xx = Xx < 0 ? random.Next(1, speed + 1) : random.Next(-speed, 0);
-				//Xx = Xx < 0 ? Xx = -Xx : Xx = -Xx;
-				//if (Xx < 0)
-				//	Xx = -Xx;
-				//else
-				//{
-				//	Xx = -Xx;
-					
-				//}
 			}
+
 			if (Picture.Location.Y + Picture.Height >= Picture.Parent.Height || Picture.Location.Y <= 0)
-			{
-				///Picture.Location = new Point(Picture.Location.X, Picture.Location.Y <= 0 ? 0 : Picture.Parent.Height - Picture.Height);
-				//Yy = Yy < 0 ? random.Next(1, speed + 1) : random.Next(-speed, 0);
-				//Yy = Yy < 0 ? Yy=+Yy : Yy=-Yy;
-				if (Yy < 0)
-					Yy = -Yy;
-				else
-					Yy = -Yy;
-			}
+				Yy = -Yy;
+			
 			Picture.Location = new Point(Picture.Location.X + Xx, Picture.Location.Y + Yy);
 		}
 	}
